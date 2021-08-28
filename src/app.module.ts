67c5b49cbfa85926e762d3de190e9cb844f6c110
pooripro <steampro35200@gmail.com>
2021-08-28 15:57:44 +0700

    [UPDATE] Module, Controller, Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { VaccinesModule } from './vaccines/vaccines.module';
+import { LocationsModule } from './locations/locations.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, VaccinesModule, LocationsModule],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
