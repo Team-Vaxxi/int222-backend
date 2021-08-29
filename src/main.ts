0822a6caf77dc8bb5427944bc92c2e38e1bd0432
pooripro <steampro35200@gmail.com>
2021-08-29 15:50:06 +0700

    [FIXED] port nestjs

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,9 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const port = process.env.DATABASE_PORT || 3306;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(port);
+  await app.listen(3000);
 }
 bootstrap();
