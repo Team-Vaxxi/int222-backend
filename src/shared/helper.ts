ddb43d45c0fea28d91855a673a32ebc302004864
pooripro <steampro35200@gmail.com>
2021-10-13 22:20:07 +0700

    [ADD] generate 96 character of image

--- src/shared/helper.ts
+++ src/shared/helper.ts
@@ -1,14 +1,23 @@
 export class Helper {
     static customFileName(req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      let uniqueSuffix = '';
+      let length = 96;
+      let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      let charactersLength = characters.length;
+      for (let index = 0; index < length; index++) {
+        uniqueSuffix += characters.charAt((Math.floor(Math.random() * 
+        charactersLength)))
+      }
       let fileExtension = "";
       if(file.mimetype.indexOf("jpeg") > -1){
           fileExtension = "jpg"
       }else if(file.mimetype.indexOf("png") > -1){
           fileExtension = "png";
       }
-      const originalName = file.originalname.split(".")[0];
-      cb(null, originalName + '-' + uniqueSuffix+"."+fileExtension);
+      // const originalName = file.originalname.split(".")[0];
+      // cb(null, originalName + '-' + uniqueSuffix + "." + fileExtension);
+      cb(null, uniqueSuffix +"." +fileExtension);
     }
    
     static destinationPath(req, file, cb) {
