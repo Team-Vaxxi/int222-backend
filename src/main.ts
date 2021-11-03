496b4de105b9647bc360691d382ae8da60fd02e0
RewBugLag <rewrew2000@gmail.com>
2021-11-04 00:50:36 +0700

    [TEMP] Removed origin in enableCORS

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    // origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: 'GET,PUT,POST,DELETE',
   });
